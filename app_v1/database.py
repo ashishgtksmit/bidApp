57f95d49803e722af62244796b05e9456e69909a
ashishgtksmit <82649691+ashishgtksmit@users.noreply.github.com>
2025-11-05 15:05:46 +0530

    db changes

--- app_v1/database.py
+++ app_v1/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from urllib.parse import quote_plus
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
@@ -17,9 +18,21 @@ DB_NAME = os.getenv("DB_NAME")
 # Build SQLAlchemy URL
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                       pool_pre_ping=True,
-    connect_args={"ssl": {"ca": "DigiCertGlobalRootG2.crt.pem"}},)
+APP_DIR = Path(__file__).resolve().parent
+CA_PATH = APP_DIR.parent / "DigiCertGlobalRootG2.crt.pem" 
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL,
+#                        pool_pre_ping=True,
+#     connect_args={"ssl": {"ca": "DigiCertGlobalRootG2.crt.pem"}},)
+
+assert Path(CA_PATH).exists(), f"CA file not found at {CA_PATH}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"ssl": {"ca": str(CA_PATH)}},
+)
+
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind=engine)
 Base = declarative_base()
 
